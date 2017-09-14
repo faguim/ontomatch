a34efa112fb8cea448eb369027627a62b82dbccd
Fagner <pantoja.ti@gmail.com>
2017-09-14 11:26:59 -0300

    POST funcionando

--- src/br/unicamp/ic/lis/ontomatch/rest/Matcher.java
+++ src/br/unicamp/ic/lis/ontomatch/rest/Matcher.java
@@ -1,13 +1,13 @@
 package br.unicamp.ic.lis.ontomatch.rest;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 
@@ -20,13 +20,15 @@ import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.util.FileManager;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 import com.sun.jersey.api.json.JSONWithPadding;
 
 import br.unicamp.ic.lis.ontomatch.model.Resource;
 
 
-@Path("/intern")
+@Path("")
 public class Matcher {
 	
 	public static String ontology_dir = "resources/ontologies/";
@@ -38,20 +40,20 @@ public class Matcher {
 	public final static String XAO = "xao";
 	public final static String HFO = "hfo";
 	
-	//curl -i -X POST -H 'Content-Type: application/json' http://localhost:8080/OntoMatch/rest/intern/resource/{chest}/{0.3}/{hfo}
-	
 	@POST
-	@Path("/resource/{text}/{similarity}/{ontology}")
+	@Path("/resource")
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces({ "application/x-javascript", MediaType.APPLICATION_JSON + ";charset=utf-8" })
-	public JSONWithPadding getRotaDaLinha(@QueryParam("callback") String callback, 
-			@PathParam("text") String text,
-			@PathParam("similarity") String similarity,
-			@PathParam("ontology") String ontology) {
+	public JSONWithPadding getResource(String params) throws JSONException {
 	
-		System.out.println(text);
-		System.out.println(similarity);
+		JSONObject jsonParams = new JSONObject(params);
+
+		String similarity = jsonParams.getString("similarity");
+		String ontology = jsonParams.getString("ontology");
+		String text = jsonParams.getString("text");
+
+		
 		Resource resource = new Resource();
-		System.out.println(ontology);
 
 		StringBuffer queryString = new StringBuffer();
 		queryString.append("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
@@ -79,9 +81,54 @@ public class Matcher {
 			resource.setUri(result.get("resource").toString());
 			resource.setSimilarity(result.getLiteral("similarity").getDouble());
 		}
-		System.out.println(resource);
 		
-		return new JSONWithPadding(new GenericEntity<Resource>(resource) {}, callback);
+		return new JSONWithPadding(new GenericEntity<Resource>(resource) {});
+	}
+	
+	@POST
+	@Path("/resources")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({ "application/x-javascript", MediaType.APPLICATION_JSON + ";charset=utf-8" })
+	public JSONWithPadding getResources(String params) throws JSONException {
+		List<Resource> resources = new ArrayList<>();
+		
+		JSONObject jsonParams = new JSONObject(params);
+
+		String text = jsonParams.getString("text");
+		String n = jsonParams.getString("n");
+		String ontology = jsonParams.getString("ontology");
+		
+		StringBuffer queryString = new StringBuffer();
+		queryString.append("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
+				+ "PREFIX oboinowl: <http://www.geneontology.org/formats/oboInOwl#> "
+				+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "
+				+ "PREFIX f: <java:"+this.getClass().getPackage().getName()+".> "
+				+ "SELECT DISTINCT ?resource ?label (f:LevenshteinFilter(?label, \"" + text + "\") as ?similarity) "
+				+ "WHERE { "
+				+ " { ?resource rdfs:label ?label} "
+
+//				+ "UNION {?resource oboinowl:hasExactSynonym ?label . } "
+//				+ "UNION {?resource oboinowl:hasRelatedSynonym ?label . } "
+
+				+ "}"
+				+ "ORDER BY DESC(?similarity) LIMIT "+n);
+
+		Query sparql = QueryFactory.create(queryString.toString());
+		System.out.println(queryString);
+		QueryExecution qExec = QueryExecutionFactory.create(sparql, getModel(ontology));
+		ResultSet rs = qExec.execSelect();
+
+		while (rs.hasNext()) {
+			QuerySolution result = rs.nextSolution();
+			System.out.println(result);
+			Resource resource = new Resource();
+			resource.setLabel(result.getLiteral("label").getValue().toString());
+			resource.setUri(result.get("resource").toString());
+			resource.setSimilarity(result.getLiteral("similarity").getDouble());
+			
+			resources.add(resource);
+		}
+		return new JSONWithPadding(new GenericEntity<List<Resource>>(resources) {});
 	}
 	
 	private Model getModel(String ontology) {
--- src/br/unicamp/ic/lis/ontomatch/test/TestClient.java
+++ src/br/unicamp/ic/lis/ontomatch/test/TestClient.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.ws.rs.core.MediaType;
 
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
@@ -17,6 +18,7 @@ import com.sun.jersey.api.json.JSONConfiguration;
 public class TestClient {
 	public static void main(String[] args) throws IOException, JSONException {
 		getResource();
+		getResources();
 	}
 
 	private static void getResource() throws IOException, JSONException{
@@ -41,12 +43,42 @@ public class TestClient {
 		JSONObject responseJson = new JSONObject(responseBody);
 		System.out.println("Response Json: "+ responseJson);
 
-
 		JSONObject resourceJSON = responseJson.getJSONObject("entity");
 
 		System.out.println("Resource: " + resourceJSON);
 		System.out.println("URI: " + resourceJSON.get("uri"));
+	}
+	
+	private static void getResources() throws IOException, JSONException{
+		String params = "{text:Chest Pain, n:5, ontology:hfo}";
+		JSONObject jsonParams = new JSONObject(params);
+
+		System.out.println(jsonParams);
+		ClientConfig clientConfig = new DefaultClientConfig();
+		clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+
+		Client client = Client.create(clientConfig);
+		WebResource webResource = client.resource("http://localhost:8080/OntoMatch/rest/resources");
+
+		ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).post(ClientResponse.class, jsonParams);
+
+		if (response.getStatus() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "+ response.getStatus());
+		}
+		String responseBody = response.getEntity(String.class);
+
+		System.out.println("Response: " + responseBody);
+		JSONObject responseJson = new JSONObject(responseBody);
+		System.out.println("Response Json: "+ responseJson);
+
+		JSONArray resourcesJSON = responseJson.getJSONArray("entity");
+
+		System.out.println("Resources: " + resourcesJSON);
+		
+		for (int i = 0; i < resourcesJSON.length(); i++) {
+			System.out.println("label: " + ((JSONObject)resourcesJSON.get(i)).get("label"));
+		}
+		
 
 	}
-
 }
