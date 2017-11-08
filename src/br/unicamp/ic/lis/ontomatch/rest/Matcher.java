c53e5d558cf78f09e17eb1401c667fe2072e1760
Fagner <pantoja.ti@gmail.com>
2017-11-08 14:13:49 -0200

    Novo método post

--- src/br/unicamp/ic/lis/ontomatch/rest/Matcher.java
+++ src/br/unicamp/ic/lis/ontomatch/rest/Matcher.java
@@ -2,6 +2,7 @@ package br.unicamp.ic.lis.ontomatch.rest;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -126,6 +127,59 @@ public class Matcher {
 			resources.add(resource);
 		}
 		return resources;
+	}
+	
+	@POST
+	@Path("/wholeresource")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Resource getWholeResource(String params) throws JSONException {
+	
+		JSONObject jsonParams = new JSONObject(params);
+
+		String similarity = jsonParams.getString("similarity");
+		String ontology = jsonParams.getString("ontology");
+		String text = jsonParams.getString("text");
+
+		
+		Resource resource = new Resource();
+
+		StringBuffer queryString = new StringBuffer();
+		queryString.append("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
+				+ "PREFIX oboinowl: <http://www.geneontology.org/formats/oboInOwl#> "
+				+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> "
+				+ "PREFIX f: <java:"+this.getClass().getPackage().getName()+".> "
+				+ "SELECT DISTINCT ?property "
+				+ "WHERE { ");
+
+		queryString.append("?resource rdfs:label ?label FILTER (f:LevenshteinFilter(?label, \"" + text + "\") >= "+ similarity + ") .");
+		queryString.append("?resource ?property ?value ");
+//				+ similarity + " ");
+
+//		queryString.append("UNION {?resource oboinowl:hasExactSynonym ?label . FILTER (f:LevenshteinFilter(?label, \""
+//				+ text + "\") >= " + similarity + ")} ");
+//		queryString.append("UNION {?resource oboinowl:hasRelatedSynonym ?label . FILTER (f:LevenshteinFilter(?label, \""
+//				+ text + "\") >= " + similarity + ")} ");
+		queryString.append("} ");
+//				+ "ORDER BY DESC(f:LevenshteinFilter(?label, \"" + text + "\")) LIMIT 1");
+System.out.println(queryString);
+		Query sparql = QueryFactory.create(queryString.toString());
+		QueryExecution qExec = QueryExecutionFactory.create(sparql, getModel(ontology));
+		ResultSet rs = qExec.execSelect();
+		while (rs.hasNext()) {
+			QuerySolution result = rs.nextSolution();
+			Iterator<String> variables = result.varNames();
+//			while (variables.hasNext()) {
+//				System.out.println(variables.next());
+////				QuerySolution querySolution = (QuerySolution) rs.next();
+//				
+//			}
+			System.out.println(result.get("property"));
+//			resource.setUri(result.get("resource").toString());
+//			resource.setSimilarity(result.getLiteral("similarity").getDouble());
+		}
+		
+		return resource;
 	}
 	
 	private Model getModel(String ontology) {
--- src/br/unicamp/ic/lis/ontomatch/test/TestClient.java
+++ src/br/unicamp/ic/lis/ontomatch/test/TestClient.java
@@ -17,20 +17,22 @@ import com.sun.jersey.api.json.JSONConfiguration;
 
 public class TestClient {
 	public static void main(String[] args) throws IOException, JSONException {
-		getResource();
-		//getResources();
+//		getResource();
+//		getResources();
+		getWholeResource();
 	}
 
 	private static void getResource() throws IOException, JSONException{
 		String params = "{text:Chest Pain, similarity:0.7, ontology:hfo}";
 		JSONObject jsonParams = new JSONObject(params);
 
-		System.out.println(jsonParams);
+//		System.out.println(jsonParams);
 		ClientConfig clientConfig = new DefaultClientConfig();
 		clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
 
 		Client client = Client.create(clientConfig);
-		WebResource webResource = client.resource("http://ontomatch.lis.ic.unicamp.br/api/rest/resource");
+		WebResource webResource = client.resource("http://localhost:8080/OntoMatch/rest/resource");
+//		WebResource webResource = client.resource("http://ontomatch.lis.ic.unicamp.br/api/rest/resource");
 
 		ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).post(ClientResponse.class, jsonParams);
 
@@ -39,21 +41,21 @@ public class TestClient {
 		}
 		String responseBody = response.getEntity(String.class);
 
-		System.out.println("Response: " + responseBody);
+//		System.out.println("Response: " + responseBody);
 		JSONObject responseJson = new JSONObject(responseBody);
-		System.out.println("Response Json: "+ responseJson);
+//		System.out.println("Response Json: "+ responseJson);
 
-		JSONObject resourceJSON = responseJson.getJSONObject("entity");
-
-		System.out.println("Resource: " + resourceJSON);
-		System.out.println("URI: " + resourceJSON.get("uri"));
+//		JSONObject resourceJSON = responseJson.getJSONObject("entity");
+//
+//		System.out.println("Resource: " + resourceJSON);
+//		System.out.println("URI: " + resourceJSON.get("uri"));
 	}
 	
 	private static void getResources() throws IOException, JSONException{
 		String params = "{text:Chest Pain, n:5, ontology:hfo}";
 		JSONObject jsonParams = new JSONObject(params);
 
-		System.out.println(jsonParams);
+//		System.out.println(jsonParams);
 		ClientConfig clientConfig = new DefaultClientConfig();
 		clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
 
@@ -67,18 +69,46 @@ public class TestClient {
 		}
 		String responseBody = response.getEntity(String.class);
 
-		System.out.println("Response: " + responseBody);
-		JSONObject responseJson = new JSONObject(responseBody);
-		System.out.println("Response Json: "+ responseJson);
-
-		JSONArray resourcesJSON = responseJson.getJSONArray("entity");
-
-		System.out.println("Resources: " + resourcesJSON);
-		
-		for (int i = 0; i < resourcesJSON.length(); i++) {
-			System.out.println("label: " + ((JSONObject)resourcesJSON.get(i)).get("label"));
-		}
+//		System.out.println("Response: " + responseBody);
+//		JSONObject responseJson = new JSONObject(responseBody);
+//		System.out.println("Response Json: "+ responseJson);
+//
+//		JSONArray resourcesJSON = responseJson.getJSONArray("entity");
+//
+//		System.out.println("Resources: " + resourcesJSON);
 //		
+//		for (int i = 0; i < resourcesJSON.length(); i++) {
+//			System.out.println("label: " + ((JSONObject)resourcesJSON.get(i)).get("label"));
+//		}
+//		
+	}
+	
+	private static void getWholeResource() throws IOException, JSONException{
+		String params = "{text:Chest Pain, similarity:0.7, ontology:hfo}";
+		JSONObject jsonParams = new JSONObject(params);
 
+//		System.out.println(jsonParams);
+		ClientConfig clientConfig = new DefaultClientConfig();
+		clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+
+		Client client = Client.create(clientConfig);
+		WebResource webResource = client.resource("http://localhost:8080/OntoMatch/rest/wholeresource");
+//		WebResource webResource = client.resource("http://ontomatch.lis.ic.unicamp.br/api/rest/resource");
+
+		ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).post(ClientResponse.class, jsonParams);
+
+		if (response.getStatus() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "+ response.getStatus());
+		}
+		String responseBody = response.getEntity(String.class);
+
+//		System.out.println("Response: " + responseBody);
+		JSONObject responseJson = new JSONObject(responseBody);
+//		System.out.println("Response Json: "+ responseJson);
+
+//		JSONObject resourceJSON = responseJson.getJSONObject("entity");
+//
+//		System.out.println("Resource: " + resourceJSON);
+//		System.out.println("URI: " + resourceJSON.get("uri"));
 	}
 }
